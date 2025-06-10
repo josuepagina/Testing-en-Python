4f84d13da796c689a1c9359ea4e8b522ae6bd74a
josuepagina <josuepagina3@gmail.com>
2025-06-10 14:52:11 -0400

    Initial commit: Adding calculator and tests

---
+++ test/test_calculator.py
@@ -0,0 +1,12 @@
+import unittest
+
+from src.calculator import suma, resta
+
+class CalculatorTest(unittest.TestCase):
+   
+    def test_sum(self):
+        self.assertEqual(suma(2, 3), 5)
+
+    def test_resta(self):
+        self.assertEqual(resta(10, 5), 5)
+
